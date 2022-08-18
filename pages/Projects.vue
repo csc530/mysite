73a0be116c476bef3ee7c3d9d4f2e2e9f400bfc8
csc530 <cscousin@lakeheadu.ca>
2022-08-17 23:56:15 -0400

    :sparkles: feat(pages): Add repo fetch from github
    Completed fetching from my repos without having my apikey exposed in the frontend😁😇😭

--- pages/Projects.vue
+++ pages/Projects.vue
@@ -1,41 +1,39 @@
 <template>
-	<!--	<base-columns :data="publicRepos" :header="{text: 'Github Projects', level: 2}" :bulma="['is-centered','is-multiline']">-->
-	<!--		<template v-slot="item">-->
-	<!--			<base-card>-->
-	<!--				<template #header>-->
-	<!--					<a :href="item.url" target="_blank"><h2>{{ item.name }}</h2></a>-->
-	<!--				</template>-->
-	<!--				<p>{{ item.description }}</p>-->
-	<!--				<template #footer="item" >-->
-	<!--					<p>{{item.stars}}</p>-->
-	<!--				</template>-->
-	<!--			</base-card>-->
-	<!--		</template>-->
-	<!--	</base-columns>-->
-	<main>poop</main>
-	<!--	Columns of the private repos-->
-	<!--	<base-columns :data="privateRepos" :header="{text: 'Private Projects & school work', level: 2}" :bulma="['is-centered','is-multiline']">-->
-	<!--		<template v-slot="item">-->
-	<!--			<base-card>-->
-	<!--				<template #header>-->
-	<!--					<a :href="item.url" target="_blank"><h2>{{ item.name }}</h2></a>-->
-	<!--				</template>-->
-	<!--				<p>{{ item.description }}</p>-->
-	<!--			</base-card>-->
-	<!--		</template>-->
-	<!--	</base-columns>-->
+	<main>
+		<base-columns :bulma="['is-centered','is-multiline']" :data="publicRepos"
+		              :header="{text: 'Github Projects', level: 2}">
+			<template v-slot="item">
+				<base-card>
+					<template #header>
+						<a :href="item.url" target="_blank"><h2>{{ item.name }}</h2></a>
+					</template>
+					<p>{{ item.description }}</p>
+					<template #footer="item">
+						<p>{{ item.stars }}</p>
+					</template>
+				</base-card>
+			</template>
+		</base-columns>
+		Columns of the private repos
+		<base-columns :bulma="['is-centered','is-multiline']" :data="privateRepos"
+		              :header="{text: 'Private Projects & school work', level: 2}">
+			<template v-slot="item">
+				<base-card>
+					<template #header>
+						<a :href="item.url" target="_blank"><h2>{{ item.name }}</h2></a>
+					</template>
+					<p>{{ item.description }}</p>
+				</base-card>
+			</template>
+		</base-columns>
+	</main>
 </template>
 <style scoped></style>
 <script lang="ts" setup>
 	/// Octokit.js	// https://github.com/octokit/core.js#readme
-	import {definePageMeta} from "#imports";
+	import {BaseCard, BaseColumns} from "#components";
+	import {useAsyncData} from "#imports";
 	
-	definePageMeta({
-		title: "Projects",
-		description: "Projects I have worked on",
-		image: "https://avatars0.githubusercontent.com/u/17098?s=460&v=4",
-		middleware: ["projects"]
-	});
-
-
+	const {data: projects} = await useAsyncData(async () => $fetch('/api/projects'))
+	const {public: publicRepos, private: privateRepos} = projects.value;
 </script>
