8e1fcb9e9d84ff4dea9b8f19539a5a712d3be605
csc530 <cscousin@lakeheadu.ca>
2022-08-17 20:41:03 -0400

    :wrench: chore(project): Update configs
    Added reactivity transforms and updated some of the site head data. Also remove .idea config files from git

--- app.vue
+++ app.vue
@@ -1,6 +1,24 @@
 <template>
-  <div>
-    <!-- <NuxtWelcome /> -->
-    <RouterView />
-  </div>
+	<div>
+		<!--     <NuxtWelcome />-->
+		<NuxtPage />
+		<!--	  <h1>Hello</h1>-->
+	</div>
 </template>
+
+<script lang="ts" setup>
+	import {useHead} from "#imports";
+	
+	useHead({
+		title: 'Nuxt.js',
+		meta: [
+			{
+				name: 'description',
+				content: 'Nuxt.js project'
+			}
+		],
+		link: [
+			{href: 'https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css', rel: 'stylesheet'}
+		]
+	})
+</script>
