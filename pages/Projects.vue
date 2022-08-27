9143a4c3bce2a81d7d0011c752778b1090879de2
csc530 <cscousin@lakeheadu.ca>
2022-08-27 00:11:48 -0400

    :zap: perf(pages): lazy load of projects
    remove navguard for fetch to complete and added watcher for lazy load of projects from server api

--- pages/Projects.vue
+++ pages/Projects.vue
@@ -1,10 +1,9 @@
 <template>
 	<main>
-		<app-columns :bulma="['is-centered','is-multiline']" :data="publicRepos"
-		             :header="{text: 'Github Projects', level: 2}">
+		<app-columns v-if="publicRepos" :bulma="['is-centered','is-multiline']" :data="publicRepos" :header="{text: 'Github Projects', level: 2}">
 			<template v-slot="item">
 				<app-card>
-					<template #header>
+					<template v-slot:header>
 						<a :href="item.url" target="_blank"><h2>{{ item.name }}</h2></a>
 					</template>
 					<p>{{ item.description }}</p>
@@ -14,8 +13,8 @@
 				</app-card>
 			</template>
 		</app-columns>
-		Columns of the private repos
-		<app-columns :bulma="['is-centered','is-multiline']" :data="privateRepos"
+		<div class="is-loading" v-else>Loading...</div>
+		<app-columns v-if="privateRepos" :bulma="['is-centered','is-multiline']" :data="privateRepos"
 		             :header="{text: 'Private Projects & school work', level: 2}">
 			<template v-slot="item">
 				<app-card>
@@ -26,13 +25,22 @@
 				</app-card>
 			</template>
 		</app-columns>
+		<div class="is-loading" v-else>Loading...</div>
+	
 	</main>
 </template>
 <style scoped></style>
 <script lang="ts" setup>
 	/// Octokit.js	// https://github.com/octokit/core.js#readme
-	import {useAsyncData} from "#imports";
+	import {useLazyAsyncData, watch} from "#imports";
+	import {AppCard,AppColumns} from "#components";
 	
-	const {data: projects} = await useAsyncData(async () => $fetch('/api/projects'))
-	const {public: publicRepos, private: privateRepos} = projects.value;
+	const {data: projects} = $(useLazyAsyncData('projects',()=> $fetch('/api/projects')));
+	let publicRepos, privateRepos;
+	watch(()=>projects,()=> {
+		if(!projects)return
+		publicRepos = projects.public;
+		privateRepos = projects.private;
+		console.log('projects changed');
+	},{deep: true,immediate: true});
 </script>
--- pages/employment/Skills.vue
+++ pages/employment/Skills.vue
@@ -1,5 +1,5 @@
 <template>
-
+		<iframe allowfullscreen height="700" loading="eager" name="resume" class="is-flex-grow-1 is-large has-ratio" title="my resume" src="/resume.PDF" width="100%"></iframe>
 </template>
 
 <script setup>
