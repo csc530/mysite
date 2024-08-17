85fbf8b9d5f807d562cc6bd00e15f1f62f2d3d68
csc530 <77406318+csc530@users.noreply.github.com>
2024-08-17 10:29:46 -0400

    ⚰️ update favicon
    remove ununsed files

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-        <header>
-            <TheNav />
-        </header>
-        <main class="container is-fluid">
-            <RouterView />
-        </main>
-        <TheFoot />
+    <header>
+        <TheNav />
+    </header>
+    <main class="container is-fluid">
+        <RouterView />
+    </main>
+    <TheFoot />
     <SpeedInsights />
 </template>
 
@@ -21,4 +21,7 @@
     import TheNav from "@/components/TheNav.vue";
     import TheFoot from "@/components/TheFoot.vue";
     import { SpeedInsights } from "@vercel/speed-insights/vue"
+    import { useFavicon } from "@vueuse/core";
+    import favicon from "./assets/images/favicon.ico"
+    useFavicon(favicon, { rel: 'icon' })
 </script>
